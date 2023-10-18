f3e15a439b0e21fd3dbb3ca0177f957f43163a98
Carla Marina Apata <carla.apata@gmail.com>
2023-10-18 15:18:47 -0300

    Se agrega auth

--- src/components/IniciarSesion.jsx
+++ src/components/IniciarSesion.jsx
@@ -1,41 +1,42 @@
-import firebase from 'firebase/compat/app';
-import * as firebaseui from 'firebaseui'
 import 'firebaseui/dist/firebaseui.css'
-import { app } from '../services/firebase/firebaseConfig';
-import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { Button, Card, Form } from 'react-bootstrap';
-import { useContext } from 'react';
-import { SesionContext } from '../Context/SesionContext';
+import { useState } from 'react';
+import { getAuth } from 'firebase/auth';
+import { app } from "../services/firebase/firebaseConfig";
+import withReactContent from "sweetalert2-react-content";
+import Swal from 'sweetalert2'
+
 const auth = getAuth(app);
 
 function IniciarSesion() {
-  const {registrado,autenticacion} = useContext(SesionContext) 
+  const [registrado, setRegistrado] = useState(true)
 
-
-  const inicio = (e) => {
-    
+  const autenticacion = async (e) => {
+    e.preventDefault();
     const correo = e.target.correo.value
     const contrasena = e.target.contrasena.value
-    
-    signInWithEmailAndPassword(auth, correo, contrasena)
-      .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-      });
-  }
 
+    if (!correo.trim() || !contrasena.trim() ) {
+      const MySwal = withReactContent(Swal)
+      MySwal.fire({
+        icon: 'error',
+        title: 'Debe completar todos los campos',
+      })
+    } else {
+      if (registrado){
+        await createUserWithEmailAndPassword(auth, correo, contrasena)
+      } else {
+        await signInWithEmailAndPassword(auth, correo, contrasena)
+      }
+    }
+  }
   return(
-  registrado
-  ? <>
-    <h1 className='text-center m-5'>Iniciá Sesión</h1>
+    <>
+    <h1 className='text-center m-5'>{registrado ? "Iniciá Sesión": "Registrate"}</h1>
     <Card className='mx-auto my-5 shadow' style={{ width: '25rem', padding:'0'}}>
       <Card.Body>
-        <Form onSubmit={inicio}>
+        <Form onSubmit={autenticacion}>
           <Form.Group className="mt-3" controlId="correo">
             <Form.Control name='correo' type="email" placeholder="Correo Electrónico" />
           </Form.Group>
@@ -43,41 +44,15 @@ function IniciarSesion() {
             <Form.Control name='contrasena' type="password" placeholder="Contraseña" />
           </Form.Group>
           <div className='d-flex justify-content-end'>
-            <Button type='submit' variant='dark' className='m-2'>Iniciar</Button>
+            <Button type='submit' variant='dark' className='m-2'>{registrado ? "Iniciar": "Registrar"}</Button>
           </div>
         </Form>
+        <div className='d-flex justify-content-between m-2'>
+          <p className=' my-auto'> {registrado ?  "¿No tenés cuenta?" : "¿Ya tenés una cuenta?"} </p>
+          <Button onClick={()=> setRegistrado(!registrado)} variant='outline-dark'>{registrado ? "Registrate": "Iniciá sesión" }</Button>
+      </div>
       </Card.Body>
     </Card>
-    </>
-    : <>
-      <h1 className='text-center m-5'>Registrate</h1>
-      <Card className='mx-auto my-5 shadow' style={{ width: '25rem', padding:'0'}}>
-        <Card.Body>
-          <Form className='m-auto' onSubmit={autenticacion}>
-            <Form.Group className="mt-3" controlId="nombre">
-              <Form.Control name='nombre' type="text" placeholder="Nombre" />
-            </Form.Group>
-            <Form.Group className="mt-3" controlId="apellido">
-              <Form.Control name='apellido' type="text" placeholder="Apellido" />
-            </Form.Group>
-            <Form.Group className="mt-3" controlId="telefono">
-              <Form.Control name='telefono' type="text" placeholder="Teléfono" />
-            </Form.Group>
-            <Form.Group className="mt-3" controlId="correo">
-              <Form.Control name='correo' type="email" placeholder="Correo Electrónico" />
-            </Form.Group>
-            <Form.Group className="mt-3" controlId="contrasena">
-              <Form.Control name='contrasena' type="password" placeholder="Contraseña" />
-            </Form.Group>
-            <Form.Group className="mt-3" controlId="confirmacion">
-              <Form.Control name='confirmacion' type="password" placeholder="Repita la contraseña" />
-            </Form.Group>
-            <div className='d-flex justify-content-end'>
-              <Button type='submit' variant='dark' className='m-2'>Registrarse</Button>
-            </div>
-          </Form>
-        </Card.Body>
-      </Card>
     </>
     
   )
